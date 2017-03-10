d89404983376f88daa5696f9cf0e8bbf82ac144e
Administrateur <Administrateur@DFR000322.corp.capgemini.com>
2017-03-10 10:02:37 +0100

    Lecture de la liste des points d'extension

--- com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalUIActivator.java
+++ com.sogeti.rental.ui/src/com/sogeti/rental/ui/RentalUIActivator.java
@@ -1,5 +1,8 @@
 package com.sogeti.rental.ui;
 
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -12,14 +15,15 @@ import org.osgi.framework.FrameworkUtil;
  */
 public class RentalUIActivator extends AbstractUIPlugin implements RentalUIConstants {
 
-	// Implémente l'interface RentalUIConstants juste pour pouvoir récupérer les constantes image de l'interface (IMG_AGENCY, etc).
-	
+	// Implémente l'interface RentalUIConstants juste pour pouvoir récupérer les
+	// constantes image de l'interface (IMG_AGENCY, etc).
+
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.sogeti.rental.ui"; //$NON-NLS-1$
 
 	// The shared instance
 	private static RentalUIActivator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -28,16 +32,33 @@ public class RentalUIActivator extends AbstractUIPlugin implements RentalUIConst
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
+	 * BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+
+		readVueExtension();
+	}
+
+	private void readVueExtension() {
+		// Read all configurations
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		for (IConfigurationElement e : reg.getConfigurationElementsFor("org.eclipse.ui.views")) {
+			// On ne veut pas les element "category" et "stickyView"
+			if (e.getName().equals("view"))
+				// Affiche la liste de view avec leur nom et le nom du plugin auquel la vue appartient
+				System.out.println(e.getName() + " " + e.getAttribute("name") + " " + e.getNamespaceIdentifier());
+		}
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.
+	 * BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -52,16 +73,21 @@ public class RentalUIActivator extends AbstractUIPlugin implements RentalUIConst
 	public static RentalUIActivator getDefault() {
 		return plugin;
 	}
-	
+
 	@Override
 	protected void initializeImageRegistry(ImageRegistry reg) {
 		super.initializeImageRegistry(reg);
-		
+
 		// Récupère le bundle de ce plugin
-		
-		// Un plugin est un bundle qui a en plus les onglets Extensions et Points d'extension, spécifiques à Eclipse
-		Bundle b = FrameworkUtil.getBundle(this.getClass()); // On peut mettre ici n'importe quelle classe du plugin en paramètre
-		
+
+		// Un plugin est un bundle qui a en plus les onglets Extensions et
+		// Points d'extension, spécifiques à Eclipse
+		Bundle b = FrameworkUtil.getBundle(this.getClass()); // On peut mettre
+																// ici n'importe
+																// quelle classe
+																// du plugin en
+																// paramètre
+
 		reg.put(IMG_AGENCY, ImageDescriptor.createFromURL(b.getEntry(IMG_AGENCY)));
 		reg.put(IMG_RENTAL, ImageDescriptor.createFromURL(b.getEntry(IMG_RENTAL)));
 		reg.put(IMG_OBJECT, ImageDescriptor.createFromURL(b.getEntry(IMG_OBJECT)));
